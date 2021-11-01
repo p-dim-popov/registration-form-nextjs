d136dbce04e3a3e1d2088120f1f8210cb4ba1f73
Peter Popov <p.dim.popov@gmail.com>
2021-11-01 17:01:29 +0200

    feat(RegisterFormHeader): implement navigation buttons

---
+++ src/components/register/RegisterFormHeader.spec.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import RegisterFormHeader from "@src/components/register/RegisterFormHeader";
+
+describe("RegisterFormHeader", () => {
+  it("should have three buttons", () => {
+    render(<RegisterFormHeader />);
+
+    const buttons = screen.queryAllByText(/[123]/i);
+    expect(buttons).toHaveLength(3);
+  });
+});
---
+++ src/components/register/RegisterFormHeader.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const RegisterFormHeader: React.FC = (props) => {
+  return (
+    <div>
+      <div className="flex flex-row">
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterFormHeader;
