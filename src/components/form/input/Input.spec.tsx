652aa61c5281d64e625a25297c4e9378a1f6017f
Peter Popov <p.dim.popov@gmail.com>
2021-11-02 12:33:09 +0200

    test(Input): should receive props

--- src/components/form/input/Input.spec.tsx
+++ src/components/form/input/Input.spec.tsx
@@ -4,8 +4,12 @@ import Input from "@src/components/form/input/Input";
 
 describe("Input", () => {
     it("should render input field", () => {
-        const { container } = render(<Input />);
+        const { container } = render(<Input id="TEST" />);
 
         expect(container.querySelector("input")).toBeInTheDocument();
+    });
+
+    it("should have props: id, <IUseValidationOptions>validation?, showValidationStatus?, label?", () => {
+        render(<Input<string> id="Name" validation={{ rules: [] }} label="Full Name" />);
     });
 });
--- src/components/form/input/Input.tsx
+++ src/components/form/input/Input.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { IUseValidationOptions } from "@src/hooks/useValidation";
 
-const Input: React.FC = () => {
+export interface IInputProps<T> {
+    id: string;
+    label?: string;
+    validation?: IUseValidationOptions<T>;
+    showValidationStatus?: boolean;
+}
+
+function Input<T>(props: React.PropsWithChildren<IInputProps<T>>) {
     return <input />;
-};
+}
 
 export default Input;
