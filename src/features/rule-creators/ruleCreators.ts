4d392e82681782c5e6ec3375ac337a5280339bb1
Peter Popov <p.dim.popov@gmail.com>
2021-11-03 11:57:03 +0200

    feat(ruleCreators): implement selection from context

--- src/features/rule-creators/ruleCreators.ts
+++ src/features/rule-creators/ruleCreators.ts
@@ -9,16 +9,26 @@ const capitalizeFirst = (value: string) => `${value[0].toLocaleUpperCase()}${val
 
 export const createMessage = (options: ICreateMessageOptions, defaultCreator: (field: string) => string) => options.message || capitalizeFirst(defaultCreator(options.name || "field"));
 
-const createField = <T, TContext = undefined>(options: ICreateMessageOptions = {}) => ({
+const createField = <T, TContext = null>(options: ICreateMessageOptions = {}) => ({
     isRequired: [
         (value: T) => !!value, createMessage(options, (field: string) => `${field} is required!`),
     ] as IUseValidationRule,
-    isEqualOrGreaterThan: (number: number): IUseValidationRule => [
-        ((value: T) => +value >= number), createMessage(options, (field: string) => `${field} should be equal or greater than ${number}!`),
-    ],
-    isDifferentThan: (selector: (context: TContext) => T, description: string) => [
-        ((value: T, context: TContext) => value !== selector(context)), createMessage(options, (field: string) => `${field} should be different than ${description}`),
-    ] as IUseValidationRule<TContext>,
+    isEqualOrGreaterThan: (number: T | ((context: TContext) => T), description?: string) => {
+        const isSelector = typeof number === "function";
+        const selector = (<Function>(isSelector ? number : () => number));
+        return [
+            ((value: T, context: TContext) => value >= selector(context)),
+            createMessage(options, (field: string) => `${field} should be equal or greater than ${isSelector ? description : number}!`),
+        ] as IUseValidationRule<TContext>;
+    },
+    isDifferentThan: (otherValue: T | ((context: TContext) => T), description?: string) => {
+        const isSelector = typeof otherValue === "function";
+        const selector = (<Function>(isSelector ? otherValue : () => otherValue));
+        return [
+            ((value: T, context: TContext) => value !== selector(context)),
+            createMessage(options, (field: string) => `${field} should be different than ${isSelector ? description : otherValue}`),
+        ] as IUseValidationRule<TContext>;
+    },
 });
 
 export const genericField = createField<string>();
