168a3fb23d62256d6316007475e3542bf65575ea
Peter Popov <p.dim.popov@gmail.com>
2021-11-04 15:50:22 +0200

    refactor(.eslintrc): disable eslint rule jsx no props spread

--- src/components/input/Input.spec.tsx
+++ src/components/input/Input.spec.tsx
@@ -31,7 +31,6 @@ describe("Input", () => {
         [{ id: "TEST" }, "TEST"],
         [{ id: "TEST", label: "TEST_LABEL" }, "TEST_LABEL"],
     ])("should show correct placeholder: (%s) (%s)", (props, expectedLabel) => {
-        // eslint-disable-next-line react/jsx-props-no-spreading
         render(<Input {...props} inlineLabel />);
 
         const input = screen.queryByPlaceholderText(expectedLabel);
