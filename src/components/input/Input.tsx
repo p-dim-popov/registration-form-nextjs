acec5bee8711d0e3d1e2193d92dfbbdeb626f61a
Peter Popov <p.dim.popov@gmail.com>
2021-11-05 11:24:13 +0200

    feat(IHaveValidation): extract

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,21 +1,20 @@
 import React from "react";
-import useValidation, { IUseValidationOptions } from "@src/hooks/useValidation/useValidation";
+import useValidation from "@src/hooks/useValidation/useValidation";
 import useFormContextDefinitions
     from "@src/hooks/form/useFormContextDefinitions/useFormContextDefinitions";
 import { IFormContext } from "@src/contexts/form/FormContext";
 import ValidationErrors from "@src/components/validation-error/ValidationErrors";
 import ValidationStatusPreview from "@src/components/validation-status-preview/ValidationStatusPreview";
 import { IHaveLabel } from "@src/interfaces/IHaveLabel";
+import { IHaveValidation } from "@src/interfaces/IHaveValidation";
 
-export interface IInputProps<TContext extends IFormContext<string>> extends IHaveLabel {
+export interface IInputProps extends IHaveLabel, IHaveValidation<string> {
     id: string;
-    validation?: IUseValidationOptions<string, TContext>;
-    showValidationStatus?: boolean;
 }
 
 function Input<TContext extends IFormContext<string>>({
     id, label = id, name = id, validation, showValidationStatus, inlineLabel,
-}: React.PropsWithChildren<IInputProps<TContext>>) {
+}: React.PropsWithChildren<IInputProps>) {
     const validationMessages = validation?.rules.map(([,e]) => e);
     const [
         onChange, startValidating, errorMessages, status,
---
+++ src/interfaces/IHaveValidation.ts
@@ -0,0 +1,7 @@
+import { IUseValidationOptions } from "@src/hooks/useValidation/useValidation";
+import { IFormContext } from "@src/contexts/form/FormContext";
+
+export interface IHaveValidation<T, TContext extends IFormContext<T> = IFormContext<T>> {
+    validation?: IUseValidationOptions<T, TContext>;
+    showValidationStatus?: boolean;
+}
