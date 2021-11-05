193b630b32ad420bbe5d61e3ab51d289e9838b2c
Peter Popov <p.dim.popov@gmail.com>
2021-11-05 16:13:16 +0200

    refactor(account-details): show checkboxes

--- src/pages/register/account-details.tsx
+++ src/pages/register/account-details.tsx
@@ -6,6 +6,7 @@ import {
 } from "@src/contexts/register/RegisterContext";
 import Input from "@src/components/input/Input";
 import Rule from "@src/features/rule-creators/ruleCreators";
+import Checkbox from "@src/components/checkbox/Checkbox";
 
 const validations = {
     email: { rules: [Rule<string>({ name: "email" }).isRequired, Rule<string>().shouldMatch(/^.*?@.*?$/i)] },
@@ -14,44 +15,50 @@ const validations = {
     securityQuestion2: { rules: [Rule<string>().isRequired, Rule<string>().hasLengthBetween(2, 30), Rule<string>().shouldMatch(/[a-z]/i)] },
 };
 
-const AccountDetails: NextPageWithLayout = () => {
-    return (
-        <>
+const AccountDetails: NextPageWithLayout = () => (
+    <>
+        <Input
+            id="email"
+            validation={validations.email}
+            inlineLabel
+        />
+
+        <Input
+            id="password"
+            validation={validations.password}
+            inlineLabel
+            showValidationStatus
+        />
+
+        <section>
+            Security Questions
             <Input
-                id="email"
-                validation={validations.email}
+                id="question-1"
+                label="Your mother's maiden name"
+                validation={validations.securityQuestion1}
                 inlineLabel
             />
-
             <Input
-                id="password"
-                validation={validations.password}
+                id="question-2"
+                label="Your place of birth"
+                validation={validations.securityQuestion2}
                 inlineLabel
-                showValidationStatus
             />
+        </section>
 
-            <section>
-                Security Questions
-                <Input
-                    id="question-1"
-                    label="Your mother's maiden name"
-                    validation={validations.securityQuestion1}
-                    inlineLabel
-                />
-                <Input
-                    id="question-2"
-                    label="Your place of birth"
-                    validation={validations.securityQuestion2}
-                    inlineLabel
-                />
-            </section>
-
-            <section>
-                Marketing Preferences
-            </section>
-        </>
-    );
-};
+        <section>
+            Marketing Preferences
+            <Checkbox
+                id="showNameIfWon"
+                label="I am happy for my first name to be shown on the site if I win (Optional)"
+            />
+            <Checkbox
+                id="shouldSubscribeToNewsletter"
+                label="Email me about promotions, large jackpots and results (Optional)"
+            />
+        </section>
+    </>
+);
 
 AccountDetails.getLayout = (page: ReactElement) => (
     <RegisterLayout page={RegisterPage.AccountDetails}>
