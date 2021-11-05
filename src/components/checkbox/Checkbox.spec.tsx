c6f8246d2acf735b5c83d97f4d5509b9cd247b38
Peter Popov <p.dim.popov@gmail.com>
2021-11-05 12:13:58 +0200

    test(Checkbox): should have label

--- src/components/checkbox/Checkbox.spec.tsx
+++ src/components/checkbox/Checkbox.spec.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Checkbox from "@src/components/checkbox/Checkbox";
 
 describe("Checkbox", () => {
-    it("should render input", () => {
-        const { container } = render(<Checkbox />);
+    it("should render input with id", () => {
+        const { container } = render(<Checkbox id="TEST" />);
 
         expect(container.querySelector("input[type=checkbox]")).toBeInTheDocument();
+        expect(container.querySelector("#TEST")).toBeInTheDocument();
+    });
+
+    it.each([
+        ["TEST_ID", undefined, "TEST_ID"],
+        ["TEST_ID", "TEST_LABEL", "TEST_LABEL"],
+    ])("should have label", (id: string, label: string | undefined, expectedLabel: string) => {
+        render(<Checkbox id={id} label={label} />);
+
+        expect(screen.getByLabelText(expectedLabel)).toBeInTheDocument();
     });
 });
--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { IHaveLabel } from "@src/interfaces/IHaveLabel";
+import { ICanHaveValidation } from "@src/interfaces/ICanHaveValidation";
+import { ICanHaveContext } from "@src/interfaces/ICanHaveContext";
+import { IFormContext } from "@src/contexts/form/FormContext";
 
-const Checkbox: React.FC = () => (
-    <input type="checkbox" />
-);
+export interface ICheckboxProps<TContext extends IFormContext<boolean>>
+    extends IHaveLabel, ICanHaveValidation<boolean>, ICanHaveContext<TContext> {
+    id: string;
+}
+
+function Checkbox<TContext extends IFormContext<boolean>>({
+    id, label = id, name = id, inlineLabel,
+}: React.PropsWithChildren<ICheckboxProps<TContext>>) {
+    return (
+        <div>
+            {!inlineLabel && <label htmlFor={name}>{label}</label>}
+            <input
+                id={id}
+                name={name}
+                type="checkbox"
+                className="form-checkbox h-5 w-5 text-gray-600"
+            />
+        </div>
+    );
+}
 
 export default Checkbox;
