0b542aff1bf101e3c69c74a913ae6b5dde1daccc
Peter Popov <p.dim.popov@gmail.com>
2021-11-08 10:14:37 +0200

    test(Selector): click handlers

--- src/components/selector/Selector.spec.tsx
+++ src/components/selector/Selector.spec.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Selector from "@src/components/selector/Selector";
+import userEvent from "@testing-library/user-event";
 
 describe("Select", () => {
     it("should render options", () => {
@@ -23,5 +24,29 @@ describe("Select", () => {
             expect(element).toHaveAttribute("name", data.id);
         });
         expect(container.querySelectorAll("label")).toHaveLength(2);
+    });
+
+    it("should react to changes", () => {
+        const data = {
+            id: "test-selector",
+            label: "Test Selector",
+            value: "option-2",
+            onChange: jest.fn(),
+        };
+
+        const { container } = render(
+            <Selector
+                {...data}
+                definitions={[
+                    { value: "option-1", label: "Option 1" },
+                    { value: "option-2", label: "Option 2" },
+                ]}
+            />,
+        );
+
+        const option1 = container.querySelector("input[value=option-1]") as HTMLInputElement;
+        userEvent.click(option1!);
+
+        expect(data.onChange).toHaveBeenCalledWith(option1?.value);
     });
 });
--- src/components/selector/Selector.tsx
+++ src/components/selector/Selector.tsx
@@ -1,23 +1,33 @@
 import React from "react";
 import { IHaveLabel } from "@src/interfaces/IHaveLabel";
+import { ICanBeControlled } from "@src/interfaces/ICanBeControlled";
 
-export interface ISelectorProps extends IHaveLabel {
+export interface ISelectorProps extends IHaveLabel, ICanBeControlled<string | number> {
     id: string;
-    definitions: { value: string, label?: string }[]
+    definitions: { value: string, label?: string }[];
 }
 
 const Selector: React.FC<ISelectorProps> = ({
     definitions,
     id, name = id, label = id,
+    value, onChange,
 }) => (
     <>
         {label}
-        {definitions.map((definition) => (
-            <div key={definition.value}>
-                <input type="radio" value={definition.value} name={name} />
-                <label htmlFor={name}>{definition.label ?? definition.value}</label>
-            </div>
-        ))}
+        {definitions.map((definition) => {
+            return (
+                <label htmlFor={name} key={definition.value}>
+                    <input
+                        type="radio"
+                        value={definition.value}
+                        name={name}
+                        checked={value === definition.value}
+                        onChange={() => onChange?.(definition.value)}
+                    />
+                    {definition.label ?? definition.value}
+                </label>
+            );
+        })}
     </>
 );
 
