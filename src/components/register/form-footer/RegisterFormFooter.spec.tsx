0dc9b32689fe7ca65cb6a0178508cf865736b880
Peter Popov <p.dim.popov@gmail.com>
2021-11-08 14:54:09 +0200

    test(RegisterFormFooter): layout

---
+++ src/components/register/form-footer/RegisterFormFooter.spec.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import RegisterFormFooter from "@src/components/register/form-footer/RegisterFormFooter";
+
+describe("RegisterFormFooter", () => {
+    it("should have continue button and contact us link separated by line", () => {
+        const { container } = render(<RegisterFormFooter />);
+
+        const button = container.querySelector("button");
+        expect(button).toBeInTheDocument();
+        const separator = button!.nextElementSibling;
+        expect(separator).toBeInstanceOf(HTMLDivElement);
+        expect(separator).toHaveClass("border-t-2 border-gray-200 min-w-full self-baseline my-8");
+        expect(separator).toBeInTheDocument();
+        expect(button!.nextElementSibling).toBeInTheDocument();
+        expect(container.querySelector("a")).toBeInTheDocument();
+    });
+});
--- src/components/register/form-footer/RegisterFormFooter.tsx
+++ src/components/register/form-footer/RegisterFormFooter.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 const RegisterFormFooter: React.FC = () => (
-    <footer>
-        <button type="button">Continue</button>
-        {/* This is intended to be opened as external link */}
-        <a target="_blank" href="/contact-us">Contact us</a>
-    </footer>
+    <div className="flex flex-col items-center">
+        <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-44 rounded-full">Continue</button>
+        <div className="border-t-2 border-gray-200 min-w-full self-baseline my-8" />
+        <div>
+            Need help?
+            {" "}
+            <a target="_blank" href="/contact-us">Contact us</a>
+        </div>
+    </div>
 );
 
 export default RegisterFormFooter;
