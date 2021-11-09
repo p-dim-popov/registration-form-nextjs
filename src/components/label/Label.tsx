e3538c3ffb8cd98f09b4b78948b015697c724aa9
Peter Popov <p.dim.popov@gmail.com>
2021-11-09 16:23:58 +0200

    feat(ICanHaveOverridenClassName):

--- src/components/label/Label.tsx
+++ src/components/label/Label.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import { ICanBeHidden } from "@src/interfaces/ICanBeHidden";
+import { ICanHaveOverriddenClassName } from "@src/interfaces/ICanHaveOverriddenClassName";
+import classNames from "classnames";
 
-export interface ILabelProps extends ICanBeHidden {
+export interface ILabelProps extends ICanBeHidden, ICanHaveOverriddenClassName {
     htmlFor: string;
     label: string;
 }
 
-const Label: React.FC<ILabelProps> = ({ isHidden, htmlFor, label }) => (
-    !isHidden ? <label htmlFor={htmlFor}>{label}</label> : null
+const Label: React.FC<ILabelProps> = ({
+    isHidden, htmlFor, label, className,
+}) => (
+    !isHidden ? (
+        <label
+            htmlFor={htmlFor}
+            className={classNames({
+                "font-semibold": !className,
+                [className]: !!className,
+            })}
+        >
+            {label}
+        </label>
+    ) : null
 );
 
 export default Label;
