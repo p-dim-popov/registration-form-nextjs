0ce7e1f702a6ea70204d951a7c053c4ec55aa045
Peter Popov <p.dim.popov@gmail.com>
2021-11-09 16:28:23 +0200

    feat(ICanMergeClassNames):

--- src/components/label/Label.tsx
+++ src/components/label/Label.tsx
@@ -2,21 +2,22 @@ import React from "react";
 import { ICanBeHidden } from "@src/interfaces/ICanBeHidden";
 import { ICanHaveOverriddenClassName } from "@src/interfaces/ICanHaveOverriddenClassName";
 import classNames from "classnames";
+import { ICanMergeClassNames } from "@src/interfaces/ICanMergeClassNames";
 
-export interface ILabelProps extends ICanBeHidden, ICanHaveOverriddenClassName {
+export interface ILabelProps extends ICanBeHidden, ICanHaveOverriddenClassName, ICanMergeClassNames {
     htmlFor: string;
     label: string;
 }
 
 const Label: React.FC<ILabelProps> = ({
-    isHidden, htmlFor, label, className,
+    isHidden, htmlFor, label, className, shouldMergeClassNames,
 }) => (
     !isHidden ? (
         <label
             htmlFor={htmlFor}
             className={classNames({
-                "font-semibold": !className,
-                [className]: !!className,
+                "font-semibold": !className || shouldMergeClassNames,
+                [className ?? ""]: !!className,
             })}
         >
             {label}
