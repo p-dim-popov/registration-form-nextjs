8a1e0756e4d7cbd9f155614bda1ffe250c13c914
Peter Popov <p.dim.popov@gmail.com>
2021-11-11 11:01:50 +0200

    test(user-details): date of birth should be valid date

--- src/__tests__/pages/register/user-details.spec.tsx
+++ src/__tests__/pages/register/user-details.spec.tsx
@@ -3,6 +3,14 @@ import { render, screen } from "@testing-library/react";
 import UserDetails from "@src/pages/register/user-details";
 import userEvent from "@testing-library/user-event";
 
+const renderPage = () => {
+    const getLayout = UserDetails.getLayout || ((page) => page);
+    const Page = getLayout(<UserDetails />);
+    return render(Page);
+};
+
+const collectErrorElements = (container: HTMLElement) => Array.from(container.querySelectorAll("ul li em"));
+
 describe("UserDetails", () => {
     it("should show items", () => {
         render(<UserDetails />);
@@ -22,19 +30,47 @@ describe("UserDetails", () => {
                 ["last name", "valid n0t", "lastName", false],
                 ["last name", "no", "lastName", false],
             ])("%s should be only letters, spaces, apostrophes, dashes and between 2 - 30, value: %s", (label: string, value: string, fieldName: string, isValid: boolean) => {
-                const getLayout = UserDetails.getLayout || ((page) => page);
-                const Page = getLayout(<UserDetails />);
-                const { container } = render(Page);
+                const { container } = renderPage();
                 const nameInput = container.querySelector(`#${fieldName}`);
                 userEvent.type(nameInput!, value);
                 userEvent.tab();
 
-                const errorsList = container.querySelectorAll("ul li em");
+                const errorsList = collectErrorElements(container);
                 const expectErrorsListInnerHtml = expect(Array.from(errorsList).map((e) => e.innerHTML).join(""));
                 if (isValid) {
                     expectErrorsListInnerHtml.not.toMatch(new RegExp(label, "i"));
                 } else {
                     expectErrorsListInnerHtml.toMatch(new RegExp(label, "i"));
+                }
+            });
+        });
+
+        describe("date of birth", () => {
+            it.each([
+                ["1-2-1900", true],
+                ["91-22-1900", false],
+                ["2-10-", false],
+            ])("should be valid date: %s", (date: string, isValid: boolean) => {
+                const { container } = renderPage();
+                const dobInput = {
+                    day: container.querySelector("input[placeholder=DD]"),
+                    month: container.querySelector("input[placeholder=MM]"),
+                    year: container.querySelector("input[placeholder=YYYY]"),
+                };
+                const [day, month, year] = date.split("-");
+
+                userEvent.type(dobInput.day!, day);
+                userEvent.type(dobInput.month!, month);
+                userEvent.type(dobInput.year!, year);
+                userEvent.tab();
+
+                const errorElements = collectErrorElements(container);
+                const expectErrorElementsCount = expect(errorElements.length);
+
+                if (isValid) {
+                    expectErrorElementsCount.not.toBeGreaterThanOrEqual(1);
+                } else {
+                    expectErrorElementsCount.toBeGreaterThanOrEqual(1);
                 }
             });
         });
